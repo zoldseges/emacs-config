583f87a3d0caf1c76e4a2ce9489cd4053db01619
palkovics adam <pa.adam94@gmail.com>
2025-10-31 20:24:16 +0100

    turn off auto-save and backup

--- init.el
+++ init.el
@@ -4,7 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(custom-enabled-themes '(tsdh-dark))
+ '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(package-selected-packages '(magit multiple-cursors))
  '(ring-bell-function 'ignore)
