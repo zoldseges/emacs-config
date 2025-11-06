d09a1db454502c6a9ea00ba6a059bca1b6e94e0c
palkovics adam <pa.adam94@gmail.com>
2025-11-06 11:24:29 +0100

    display column number

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
+ '(column-number-mode t)
  '(custom-enabled-themes '(tsdh-dark))
  '(make-backup-files nil)
  '(menu-bar-mode nil)
