36f94b390d42e4986e7cf88c1b2acff8f9ca9a18
palkovics adam <pa.adam94@gmail.com>
2026-04-26 13:18:05 +0200

    uninstall org-roam

--- init.el
+++ init.el
@@ -13,11 +13,7 @@
  '(custom-enabled-themes '(tsdh-dark))
  '(make-backup-files nil)
  '(menu-bar-mode nil)
- '(package-selected-packages '(
-			       go-mode
-			       magit
-			       multiple-cursors
-			       org-roam))
+ '(package-selected-packages '(go-mode magit multiple-cursors))
  '(ring-bell-function 'ignore)
  '(tool-bar-mode nil)
  '(visible-bell t)
